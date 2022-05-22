95b062f02f03f2183363c76a166c1a5a49c1cc1c
Okino Kamali Leiba <17099286+OkinoLeiba@users.noreply.github.com>
2022-05-22 15:33:08 -0400

    Add files via upload

--- PASSREADACCESS.h
+++ PASSREADACCESS.h
@@ -3,4 +3,5 @@
 
 //memory safety: prevent buffer read overflow 
 template <size_t index, typename pass_len, size_t length>
-pass_len& get(&arr)[length] {static_assert(index < lenght); return arr[lenght]; };
+pass_len& get(pass_len (&arr)[length]) {static_assert(index < length, "Access Denied"); return arr[length]; }
+ 
--- practice_CreateUser_v2022.05.08.cpp
+++ practice_CreateUser_v2022.05.08.cpp
@@ -1,54 +1,89 @@
-// practice_CreateUser_v2022.05.08.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//practice_CreateUser_v2022.05.08.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 //Project Practice: practice_v2022.05.08 login and password creation 
 
 
 #include <iostream>
-#include <istream>
 #include <fstream>
 #include <string>
+#include <utility>
+#include <cstring>
+#include <vector>
 #include "PASSCHECK.h"
 #include "PASSREADACCESS.h"
 using namespace std;
 
 
 
-	
-void macro_charC(string user_name, string password)
+struct struct_pass_check 
 {
 
-	/*string& s;
-	constexpr string input_string(s);
-	//declare convert char array
-	 char charC[input_string.length()];
-
-	//convert string to char array
-	for (int i = 0; sizeof(s); i++)
-	{
-		s[i] = charC[i];
-	}*/
-
-	{
-		//convert user_name to char array
-		for (int i = 0; sizeof user_name; i++)
-		{
+		/*
 		
-			string input_string(user_name);
-			char* charC = &input_string[i];
-			string user_name[] = { charC };
-		}
-	}
 
-	//{
-	//	//convert password to char array
-	//	char* charc;
-	//	string input_string(password);
-	//	charc = &input_string[0];
-	//	string password[] = { charc };
-	//}
-}
-void func_pass_check(int pass_check)
-{
+
+			//convert user_name to char array
+			for (int i = 0; sizeof user_name; i++)
+			{
+
+				string input_string(user_name);
+				char* charC = &input_string[i];
+				string user_name[] = { charC };
+			}
+
+
+
+			//convert password to char array
+			char* charc;
+			string input_string(password);
+			charc = &input_string[0];
+			string password[] = { charc };
+		*/
+
+
+string user_name, password;
+public:
+	static char* macro_userC(string* user_name)
+	{
+		//convert user name to char array
+		string str_obj(*user_name);
+		char* char_user = &str_obj[0];
+		return char_user;
+	}
+public:
+	static char* macro_passwordC(string* password)
+	{
+		//convert password to char array
+		string str_obj(*password); //keep for conversation of other types
+		char* char_password = &str_obj[0]; 
+		return char_password;
+		
+
+
+		string& password
+		constexpr string input_string(password);
+		//declare convert char array
+		char charC[input_string.length(password)];
+
+		//convert string to char array
+		for (size_t i = 0; sizeof(password); i++)
+		{
+			password[i] = charC[i];
+		}
+	
+	}
+public:
+	static string char_convert_str(char* char_user, char* char_password)
+	{
+		//convert char to string
+		string str_user(char_user); string str_pass(char_password);
+		return str_user, str_pass;
+
+	}
+};
+
+
+ {
 	using namespace pass_check;
 
 	switch (pass_check)
@@ -62,66 +97,97 @@ void func_pass_check(int pass_check)
 	case 4: { printf("Password requires 1 non-number."); }
 	}
 }
-string func_danger(string err_prompt) { printf("Danger Will Robinson."); }
-		
+
+
+struct std_error
+{
+	static void func_danger() { printf("Danger Will Robinson."); }
+};
+
 	
+
+
 
 	int main()
 	{
-		int i = 0; char user_name, password; int func_pass_chec(); char* c;
+		int i = 0; string user_name, password; int func_pass_chec(); 
 		
-
-		
-
 
 		bool is_input;
 		{
 			//prompt user for user_name and password
-			do
-			{
-				cout << "Please Input User Name: "; cin >> user_name; cout << "Please Input Password: "; cin >> password;
-				char user_name[] = {user_name};
-				
-				
-	
-				//call function to convert argument to char array 
-				void macro_charC(string user_name, string password);
-				
-				
-
-				
-				while (isalnum(password[i]))
-				{
-					char* password = &password[i]; char* user_name = &user_name[i];
-					if (!isupper(password[i])) { func_pass_check(1); }
-					if (!isdigit(password[i])) { func_pass_check(2); }
-					if (!islower(password[i])) { func_pass_check(3); }
-					if (!ispunct(password[i])) { func_pass_check(4); }
-					i++;
-				}
-				
 			
-			} while (isalpha(user_name), isalnum(password));
+				cout << "Please Input User Name: "; cin >> user_name; cout << "Please Input Password: "; cin >> password;
+
+
+				struct_pass_check struct_pass_check;
+				
+				
+				//call function to convert argument to char array and cast to static and return char pointer
+				char* char_user_name = static_cast<char*>(struct_pass_check.macro_userC(&user_name));
+				char* char_password = static_cast<char*>(struct_pass_check.macro_passwordC(&password));
+
+				//access to provide to outer scope for file write stream
+				//auto& put_CharUserName = char_user_name;
+				
+				
+				//for (size_t i = 0; sizeof(char_password); i++)
+				
+				
+					while (isalnum(char_password[i]))
+					{
+					//char* password = &password[i]; char* user_name = &user_name[i];
+					if (!isupper(char_password[i])) { func_pass_check(1); }
+					if (!isdigit(char_password[i])) { func_pass_check(2); }
+					if (!islower(char_password[i])) { func_pass_check(3); }
+					if (!ispunct(char_password[i])) { func_pass_check(4); }
+					}
+			    
+			
+			
 		};
 
 		//output string user_name and password to file
 		fstream op;
+		ofstream output;
 		string cur_dir = "C:\\";
-		string full_path = "Users\\Owner\\source\\repos\\practice_CreateUser_v2022.05.08\practice_CreateUser_v2022.05.08\\";
+		string full_path = "Users\\Owner\\source\\repos\\practice_CreateUser_v2022.05.08\\practice_CreateUser_v2022.05.08\\";
 		string file = cur_dir + full_path + "log_in" + ".csv";
 
-		op.open(file);
+		op.open(file, ios_base::out);
 		if (op.is_open())
 		{
-			//char user[] = make_pair(user_name, password);
-			do { op.put(user_name + "," + password); } while (password != "\n");
+
+			struct_pass_check struct_pass_check;
+			//call function to convert argument to char array and cast to static and return char pointer
+			char* char_user_name = static_cast<char*>(struct_pass_check.macro_userC(&user_name));
+			char* char_password = static_cast<char*>(struct_pass_check.macro_passwordC(&password));
+
+		    /*
+			pair <string, string> user_pass;
+			user_pass = make_pair(char_user_name, char_password);
+	       */
+
+		   // get size of file
+			ifstream ifs(file, ios_base::beg);
+			ifs.seekg (0, ifs.end);
+			long size = ifs.tellg();
+			ifs.seekg(0);
+
+			// allocate memory for file content
+			char* buffer = new char[size];
+
+			output << char_user_name << ',' << char_password << '\n'; 
+			//vector< unsigned char > buffer;
+			//write some data into buffer and binary to the output stream
+			//output.write(buffer, size); 
 			op.close();
 		}
 
 		//confirmation to user 
-		if (is_input = true) { printf("User has been successfully created."); system("PAUSE"); return 0; }
-		else if (is_input = false) { printf("User was not sucessfully created."); system("PAUSE"); return 1; }
-		else {func_danger("Danger");}
+		if (is_input = true) { printf("User has been successfully created.\n"); system("PAUSE"); return 0; }
+		else if (is_input = false) { printf("User was not sucessfully created.\n"); system("PAUSE"); return 1; }
+		else {std_error::func_danger();}
 
 
 
