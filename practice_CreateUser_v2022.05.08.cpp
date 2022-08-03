1477ea6e6ecd0fbcc10546a9d4500768c4d6dd7f
Okino Kamali Leiba <17099286+OkinoLeiba@users.noreply.github.com>
2022-08-03 19:30:26 -0400

    Source (.cpp) & Header (.h) & Property Files
    Updated with new code

--- practice_CreateUser_v2022.05.08.cpp
+++ practice_CreateUser_v2022.05.08.cpp
@@ -8,70 +8,183 @@
 #include <string>
 #include <utility>
 #include <cstring>
-#include <vector>
-#include "PASSCHECK.h"
-#include "PASSREADACCESS.h"
+#include <vector> //dynamic array???
+#include <new>
+#include "PASSCHECK.h";
+#include "PASSREADACCESS.h";
 using namespace std;
 
 
 
-struct struct_pass_check 
+struct Struct_Pass_Check
 {
+private: string user_name, password;
+	/*
+		//convert user_name to char array
+		for (int i = 0; sizeof user_name; i++)
+		{
 
-		/*
-		
-
-
-			//convert user_name to char array
-			for (int i = 0; sizeof user_name; i++)
-			{
-
-				string input_string(user_name);
-				char* charC = &input_string[i];
-				string user_name[] = { charC };
-			}
+			string input_string(user_name);
+			char* charC = &input_string[i];
+			string user_name[] = { charC };
+		}
 
 
 
-			//convert password to char array
-			char* charc;
-			string input_string(password);
-			charc = &input_string[0];
-			string password[] = { charc };
-		*/
+		//convert password to char array
+		char* charc;
+		string input_string(password);
+		charc = &input_string[0];
+		string password[] = { charc };
+	*/
 
-
-string user_name, password;
 public:
-	static char* macro_userC(string* user_name)
+	inline static char* macro_userC(const string* user_name)
 	{
 		//convert user name to char array
 		string str_obj(*user_name);
 		char* char_user = &str_obj[0];
-		return char_user;
+		return &char_user[0];
 	}
 public:
-	static char* macro_passwordC(string* password)
+	inline static char* macro_passwordC(const string* password)
 	{
 		//convert password to char array
 		string str_obj(*password); //keep for conversation of other types
-		char* char_password = &str_obj[0]; 
+		char* char_password = &str_obj[0];
+
+		//return char_password; //review return variable 07/19/2022
+	}
+
+
+public:
+	static char* macro_CharC1(const string* user_name)
+	{
+		//assigning pointer to string 
+		string str_user_name = *user_name;
+
+		//constructing character array with dynamic int
+		volatile int n = str_user_name.size();
+		char* char_user = new char[n+1];
+		//int n = const_cast <int*> (n);
+
+		//copying the contents of the string to char array
+		strcpy_s(char_user, str_user_name.c_str());
+
+		return char_user;
+
+		//memory management 
+		delete char_user;
+	}
+public:
+	static char* macro_CharC1(const string* password)
+	{
+		//assigning pointer to string 
+		string str_password = *password;
+
+		//constructing character array with dynamic int
+		volatile int n = str_password.size();
+		char* char_password = new char[n+1];
+		//int n = const_cast <int*> (n);
+
+		//copying the contents of the string to char array
+		strcpy(char_password, str_password.c_str());
+
 		return char_password;
+
+		//memory managment 
+		delete char_password;
+	}
+
+
+public:
+	static char* macro_CharC2(const string* user_name)  //UNFINISHED 05/24/2022 //TODO: add password convert function
+	{
+		//function like type casting 
+		//char* str_user_name = (&user_name);
+
+		
+		//char* char_user = new char[];
+		//memcpy(&char_user, &str_user_name, strlen(char_user) + 1);
 		
 
-
-		string& password
-		constexpr string input_string(password);
-		//declare convert char array
-		char charC[input_string.length(password)];
-
-		//convert string to char array
-		for (size_t i = 0; sizeof(password); i++)
-		{
-			password[i] = charC[i];
-		}
-	
 	}
+
+public:
+	static char* macro_CharC2(const string* user_name) //Notes: assigment to conversion to string missing
+	{
+		//initialize pointer 
+		//string* user_name{};
+
+		//single-argument constructor implicit converstion to string ???
+		string input_string{ *user_name };
+
+		volatile int n = input_string.size();
+		//constructor
+		char* char_user = new char[n + 1];
+
+		//convert string to char array input each character
+		for (size_t i = 0; sizeof(char_user); i++)
+		{
+			char_user[i] = input_string[i];
+		}
+
+		//memory managment 
+		delete char_user;
+	}
+
+
+public:
+	static char* macro_CharC3(const string* password) //Notes: assigment to conversion to string missing
+	{
+		//initialize pointer 
+		//string* user_name{};
+		
+		//single-argument constructor implicit converstion to string ???
+		string input_string{ *password };
+
+		volatile int n = input_string.size();
+		//constructor
+		char* char_password = new char[n + 1];
+
+		//convert string to char array input each character
+		for (size_t i = 0; sizeof(char_password); i++)
+		{
+			char_password[i] = input_string[i];
+		}
+
+		//memory managment 
+		delete char_password;
+	}
+
+
+public:
+	const char* macro_CharC4(const string& user_name)
+	{
+
+		//assign value to string 
+		//string str_user = user_name;
+
+		//the c_str() function returns a pointer to an array
+		//null-terminated sequence of characters holds current value of the string object
+		const char* char_user = user_name.c_str();
+
+		return char_user;
+	}
+public:
+	const char* macro_CharC4(const string& password)
+	{
+		//asign value to string 
+		//string str_password = password;
+
+		//the c_str() function returns a const pointer to an array
+		//null-terminated sequence of characters holds current value of the string object
+		const char* char_password = password.c_str();
+
+		return char_password;
+	}
+
+
 public:
 	static string char_convert_str(char* char_user, char* char_password)
 	{
@@ -83,9 +196,8 @@ public:
 };
 
 
- {
-	using namespace pass_check;
-
+ 
+void funcpass_check(int pass_check) {
 	switch (pass_check)
 	{
 	case 1: { printf("Password requires 1 upper case letter."); }
@@ -110,17 +222,18 @@ struct std_error
 
 	int main()
 	{
-		int i = 0; string user_name, password; int func_pass_chec(); 
+		unsigned int i = 0; string user_name, password; unsigned int func_pass_check(unsigned int pass_check); 
 		
 
 		bool is_input;
 		{
 			//prompt user for user_name and password
 			
-				cout << "Please Input User Name: "; cin >> user_name; cout << "Please Input Password: "; cin >> password;
+				//cout << "Please Input User Name: "; cin >> user_name; cout << "Please Input Password: "; cin >> password;
+				scanf_s("Please Input User Name: ", user_name); scanf_s("Please Input Password: ", password);
+				
 
-
-				struct_pass_check struct_pass_check;
+				Struct_Pass_Check struct_pass_check;
 				
 				
 				//call function to convert argument to char array and cast to static and return char pointer
@@ -158,7 +271,7 @@ struct std_error
 		if (op.is_open())
 		{
 
-			struct_pass_check struct_pass_check;
+			Struct_Pass_Check struct_pass_check;
 			//call function to convert argument to char array and cast to static and return char pointer
 			char* char_user_name = static_cast<char*>(struct_pass_check.macro_userC(&user_name));
 			char* char_password = static_cast<char*>(struct_pass_check.macro_passwordC(&password));
@@ -171,7 +284,7 @@ struct std_error
 		   // get size of file
 			ifstream ifs(file, ios_base::beg);
 			ifs.seekg (0, ifs.end);
-			long size = ifs.tellg();
+			unsigned long size = ifs.tellg();
 			ifs.seekg(0);
 
 			// allocate memory for file content
